a2b182200ae203de56d98fdad23432a9cee5f54f
AmanUllah-99 <amankacho0001@gmail.com>
2026-01-02 22:43:51 +0500

    file understance

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,11 +1,13 @@
- 
+import Test from "./Test"
 
 function App() {
-   
-  return (
-    <h1>Chai Aur React eith vite | AK</h1>,
-    <h2> Chai With React Js Tutorials</h2>
 
+  return (
+    <>
+       <Test/>
+      <h1>Chai Aur React eith vite | AK</h1>,
+      <h2> Chai With React Js Tutorials</h2>
+    </>
   )
 }
 
---
+++ basic-react/src/Test.jsx
@@ -0,0 +1,12 @@
+///// Test file
+function Test() {
+    return(
+        <>
+        <h1>Hello world</h1>
+        <h2>Amaan</h2>
+        </>
+    )
+
+}
+ 
+export default Test
